0158b7bb0b49f89b10fb4f9d8a9700cfba166eb7
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2021-12-21 09:34:36 +0000

    Updated home.php

--- views/home.php
+++ views/home.php
@@ -3,5 +3,5 @@
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 	<p>Jean-Luc Collinet.</p>
 
-
+	<p> Pipeline OK </p>
 </div><!-- #contenu -->
